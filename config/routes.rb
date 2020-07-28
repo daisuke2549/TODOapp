799ee752353c9a5a971fa61d79ffa290892adf4c
daisuke sasaki <gerund25495641@gmail.com>
2020-07-29 05:44:59 +0900

    Deviseを導入し、フォルダーの階層を変更

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+end
+
+  
+#posts#index の posts は、これから作成する controller の名前で、
+#index は、controller に設定する関数名だ。
+
+
+
