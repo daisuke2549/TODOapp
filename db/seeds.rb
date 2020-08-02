fe18399b07b428edcef0d73e980acf2759d8dc53
daisuke sasaki <gerund25495641@gmail.com>
2020-08-02 12:46:17 +0900

    記事一覧を作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do
-Board.create[
-    title: Faker::Lorem.sentence(word_count: 5),
-    content: Faker::Lorem.sentence(word_count: 100)
-]
-end
+Board.create({title: '新しい記事だよ', content: '素晴らしい記事です。これは大変すごいです。'})
+Board.create({title: '素晴らしい記事だよ', content: 'ああああああああああああああああああああああああああああああ'})
