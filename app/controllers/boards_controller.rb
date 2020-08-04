58a19f39b118330b356bc869fa6286909ac7739d
daisuke sasaki <gerund25495641@gmail.com>
2020-08-05 06:06:33 +0900

    Board新規作成・更新・編集画面を作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,37 +2,45 @@ class BoardsController < ApplicationController
        
   def index
     @boards = Board.all
+
+  def show
+      @board = Board.find(params[:id])
+  end
     end
 
   def new
-   end
+     @board = Board.new
+  end
 
-    def show
-        @board = Board.find(params[:id])
-    end
-    
+
     def create
-        @board = current_user.boards.build(article_params)
-        if @board.save
-          redirect_to board_path(@board), notice: '保存できました'
-        else
-          flash.now[:error] = '保存に失敗しました'
-          render :new
-        end
+      @board = Board.new(board_params)
+      if @board.save
+        redirect_to board_path(@board), notice: '保存できたよ'
+      else
+        flash.now[:error] = '保存に失敗しました'
+        render :new
       end
+    end
+
 
     def edit
     @board = Board.find(params[:id])
     end
 
+    def update
+    @board = Board.find(params[:id])
+     if @board.update(board_params)
+       redirect_to board_path(@board), notice: '更新できました'
+     else
+       flash.now[:error] = '更新できませんでした'
+       render :edit
+     end
+   end
+
+
     private
     def board_params
       params.require(:board).permit(:title, :content)
     end
-
-
-  def set_board
-    @board = Board.find(params[:id])
-  end
-
 end
