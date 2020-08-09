5e4f31168786a82f4fa7593bc9af8819b7cbf3fd
daisuke2549 <67216632+daisuke2549@users.noreply.github.com>
2020-08-09 18:51:53 +0900

    Revert "Add task model"

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,34 +5,35 @@ class BoardsController < ApplicationController
   end 
 
   def show
-    @boards = Board.find(params[:id])
+      @boards = Board.find(params[:id])
   end
 
   def new
     @board = Board.new
   end
 
-  def create
+   def create
     @board = Board.new(board_params)
       if @board.save!
         redirect_to boards_path(@board), notice: '保存できました'
       else
         flash.now[:error] = '保存に失敗しました'
         render :new
-  end
+   end
 
-  def edit
+    def edit
     @board = Board.find(params[:id])
-  end
+    end
 
-  def update
+    def update
     @board = Board.find(params[:id])
-    if @board.update(board_params)
-      redirect_to board_path(@board), notice: '更新できました'
-    else
-      flash.now[:error] = '更新できませんでした'
-      render :edit
-  end
+     if @board.update(board_params)
+       redirect_to board_path(@board), notice: '更新できました'
+     else
+       flash.now[:error] = '更新できませんでした'
+       render :edit
+    end
+
 
   def destroy
     board = Board.find(params[:id])
@@ -40,12 +41,15 @@ class BoardsController < ApplicationController
     redirect_to root_path, notice: '削除に成功しました'
   end
 
-private
-  def board_params
-    params.require(:board).permit(:title, :content)
-  end
+
+    private
+    def board_params
+      params.require(:board).permit(:title, :content)
+    end
 
   def set_board
     @board = Board.find(params[:id])
   end
+end
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_014159) do
+ActiveRecord::Schema.define(version: 2020_08_03_113713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,14 +18,6 @@ ActiveRecord::Schema.define(version: 2020_08_09_014159) do
   create_table "boards", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "limit"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
