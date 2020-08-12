be682921257120233a8038aee59e8b0b5f545d90
daisuke sasaki <gerund25495641@gmail.com>
2020-08-13 08:27:17 +0900

    コメント作成画面を作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+    validates :limit, presence: true
     has_many :comments, dependent: :destroy
     # @post.comments
 end
